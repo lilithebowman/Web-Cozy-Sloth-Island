9a7f64d818574eb387ce4088679272c325c1c92c
Lilithe Bowman <lilithe.bowman@gmail.com>
2023-11-27 16:37:35 -0500

    Made collision work and sound effects too!

---
+++ Assets/Cozy Sloth Island/Scripts/DestroyColliderOnHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyColliderOnHit : MonoBehaviour {
+	public GameObject gameObjectToDestroy;
+	public AudioSource audioSource;
+	private Collider colliderToDestroy;
+	private int time = 0;
+    // Start is called before the first frame update
+    void Start() {
+        colliderToDestroy = gameObject.GetComponent<Collider>();
+    }
+
+	void Update() {
+		time++;
+		if (time > 1) {
+			colliderToDestroy.enabled = false;
+			audioSource.Play();
+			Destroy(gameObjectToDestroy);
+		}
+	}
+	void OnCollisionEnter(Collision collision) {
+		colliderToDestroy.enabled = false;
+		audioSource.Play();
+		Destroy(gameObjectToDestroy);
+    }
+}
