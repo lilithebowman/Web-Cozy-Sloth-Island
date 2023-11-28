fabd12d84d71219f8991fc6ed36fb42cc5303bfc
Lilithe Bowman <lilithe.bowman@gmail.com>
2023-11-28 16:22:38 -0500

    A lot of work on nav mesh, materials, and toggle for video and audio players

---
+++ Assets/Cozy Sloth Island/Models/DJ Deck/Scripts/PlayAudioOnClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnClick : MonoBehaviour {
+
+    public GameObject gameObject;
+    public Camera camera;
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) { // if left button pressed...
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform == gameObject.transform) {
+                    // Play the audio source
+                    if (!audioSource.isPlaying) {
+                        Debug.Log("DJ Deck is stopped, Play()-ing!");
+                        Debug.Log(audioSource.isPlaying);
+                        audioSource.Play();
+                        gameObject.GetComponent<Renderer>().materials[0].EnableKeyword("_EMISSION");
+                        gameObject.GetComponent<Renderer>().materials[1].EnableKeyword("_EMISSION");
+                    } else {
+                        Debug.Log("DJ Deck is playing, Stop()-ped!");
+                        Debug.Log(audioSource.isPlaying);
+                        audioSource.Stop();
+                        gameObject.GetComponent<Renderer>().materials[0].DisableKeyword("_EMISSION");
+                        gameObject.GetComponent<Renderer>().materials[1].DisableKeyword("_EMISSION");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Cozy Sloth Island/Models/DJ Deck/Scripts/PlayVideoOnClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideoOnClick : MonoBehaviour {
+    public GameObject gameObject;
+    public Camera camera;
+    public Texture renderTexture;
+    public Texture offStateTexture;
+
+    private bool isPlaying = false;
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+
+    void Start () {
+        videoPlayer = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+        isPlaying = videoPlayer.isPlaying;
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) { // if left button pressed...
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform == gameObject.transform) {
+                    if (!isPlaying) {
+                        // Play the video source
+                        Debug.Log("Video Player Hit - Launching Video");
+                        videoPlayer.Play();
+                        isPlaying = true;
+                        gameObject.GetComponent<Renderer>().material.SetTexture("_MainTex", renderTexture);
+                    } else {
+                        // Stop the video source
+                        Debug.Log("Video Player Hit - Stopping Video");
+                        videoPlayer.Stop();
+                        isPlaying = false;
+                        gameObject.GetComponent<Renderer>().material.SetTexture("_MainTex", offStateTexture);
+                    }
+                }
+            }
+        }
+    }
+}
